fefed88ce9801070224bced45bc9186d51ac0134
mi0308 <mi0308@naver.com>
2017-03-29 12:49:31 +0900

    Create MultiSquare.java

---
+++ MultiSquare.java
@@ -0,0 +1,36 @@
+package prof_day8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+//거듭제곱 구하기 (최종)
+public class day8_multiple {
+
+	static int MOD = 1000000007;
+	static long a;
+	static long m;
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		a = Long.parseLong(st.nextToken());
+		m = Long.parseLong(st.nextToken());
+		
+		a %= MOD;
+		long ans = 1;
+		while(m>=1){
+			if((m&1)>=1) ans = ans * a % MOD;
+			a = a * a % MOD;
+			m>>=1;
+			
+		}
+		System.out.println(ans);
+		
+		
+
+	}
+
+}
